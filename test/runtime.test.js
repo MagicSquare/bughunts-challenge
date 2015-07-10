ecbe3c51623a98c32ef937560a2ea8c7f07b08f5
Jérémy Bouny <jbouny@gmail.com>
2015-07-10 18:51:42 +0200

    Initial commit for preparing the node module

---
+++ test/runtime.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var should = require('should'),
+    Challenge = require('../lib/Challenge').Challenge;
+
+describe('Challenge', function () {
+
+  it.skip("Should test", function () {
+  });
+
+});
